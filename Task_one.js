23f0926338321dc78a8d343ef781af6b2f9b2341
marjanhasan <moonmarjanhasan@gmail.com>
2023-07-26 00:09:04 +0600

    task one done

--- Task_one.js
+++ Task_one.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const userInput = "hello world";
+const userOutput = "dlrow olleh";
+let res = "";
+for (let i = userInput.length - 1; i >= 0; i--) {
+  res += userInput[i];
+}
+console.log(res);
